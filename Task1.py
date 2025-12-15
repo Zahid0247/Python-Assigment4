e051c4465a6c29de4e3760dfb56f35581394ceb8
zahid0247 <zahid.mirza4702@gmail.com>
2025-12-15 12:28:16 +0530

    task one completed

---
+++ Task1.py
@@ -0,0 +1,16 @@
+import pathlib
+
+with open("sample.txt", "wt") as fh:
+    fh.write("This is simple file\n")
+    fh.write("it contains multipal lines")
+
+try:
+    with open("Sample.txt", "rt") as fh:
+        line1 = fh.readline()
+        print(f"line1: {line1}")
+        line2 = fh.readline()
+        print(f"line2: {line2}")
+     
+
+except FileNotFoundError:
+    print(f"file Sample.txt was not found")

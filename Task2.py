679b4a67a3db472fd4ac025b1320f57aa178c4a6
zahid0247 <zahid.mirza4702@gmail.com>
2025-12-15 14:22:06 +0530

    Task 2 code run and get the finnaly masage

--- Task2.py
+++ Task2.py
@@ -3,11 +3,14 @@ try:
         taxt1 = fh.write(input("Enter text to write into the file: "))
 except FileExistsError:
     print("")
+finally:
+    print("Data is successfuly return in the file")
         
 with open("output.txt", "at") as fh:
     fh.write('\n')
     taxt2 = fh.write(input("Enter additional text to append: "))
-    
+    print("Data successfully appended")
+
 with open("output.txt", "rt") as fh:
     print("\nFile Content:")
     print(fh.read())
